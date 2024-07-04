89517b5d1a7c52b7feff89a03e4664ced3f5cd05
noat04 <toannguyen041214@gmail.com>
2024-07-05 01:43:34 +0700

    update

--- JS/heartmove.js
+++ JS/heartmove.js
@@ -5,35 +5,35 @@
 //         snowflakes[i].style.top = `${-Math.random() * screenHeight}px`;
 //     }
 // });
-document.addEventListener('DOMContentLoaded', function() {
-    function createSnowflakes(num) {
-        const snowflakesContainer = document.querySelector('.snowflakes');
-        for (let i = 0; i < num; i++) {
-            let snowflake = document.createElement('div');
-            snowflake.className = 'snowflake';
-            snowflake.style.left = `${Math.random() * 100}vw`;
-            snowflakesContainer.appendChild(snowflake);
-        }
-    }
+// document.addEventListener('DOMContentLoaded', function() {
+//     function createSnowflakes(num) {
+//         const snowflakesContainer = document.querySelector('.snowflakes');
+//         for (let i = 0; i < num; i++) {
+//             let snowflake = document.createElement('div');
+//             snowflake.className = 'snowflake';
+//             snowflake.style.left = `${Math.random() * 100}vw`;
+//             snowflakesContainer.appendChild(snowflake);
+//         }
+//     }
 
-    let numSnowflakes = window.innerWidth <= 480 ? 20 : 50; // Giảm số lượng tuyết cho thiết bị di động
-    createSnowflakes(numSnowflakes);
+//     let numSnowflakes = window.innerWidth <= 480 ? 20 : 50; // Giảm số lượng tuyết cho thiết bị di động
+//     createSnowflakes(numSnowflakes);
 
-    let snowflakes = document.getElementsByClassName('snowflake');
-    let screenHeight = window.innerHeight;
+//     let snowflakes = document.getElementsByClassName('snowflake');
+//     let screenHeight = window.innerHeight;
 
-    // Set keyframes based on screen height and width
-    let fallDistance = window.innerWidth <= 480 ? screenHeight / 2 : screenHeight * 2;
-    let styleSheet = document.styleSheets[0];
-    styleSheet.insertRule(`
-        @keyframes snowflakes-fall {
-            0% { top: -50px; }
-            100% { top: ${fallDistance}px; }
-        }
-    `, styleSheet.cssRules.length);
+//     // Set keyframes based on screen height and width
+//     let fallDistance = window.innerWidth <= 480 ? screenHeight / 2 : screenHeight * 2;
+//     let styleSheet = document.styleSheets[0];
+//     styleSheet.insertRule(`
+//         @keyframes snowflakes-fall {
+//             0% { top: -50px; }
+//             100% { top: ${fallDistance}px; }
+//         }
+//     `, styleSheet.cssRules.length);
 
-    for (let i = 0; i < snowflakes.length; i++) {
-        snowflakes[i].style.top = `${-Math.random() * screenHeight}px`;
-    }
-});
+//     for (let i = 0; i < snowflakes.length; i++) {
+//         snowflakes[i].style.top = `${-Math.random() * screenHeight}px`;
+//     }
+// });
 
