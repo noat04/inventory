6879bbe5dba00bdda3e4d5116f8d7f8d8bce8be5
noat04 <toannguyen041214@gmail.com>
2024-07-09 15:10:50 +0700

    update

--- JS/JS.js
+++ JS/JS.js
@@ -1,7 +1,7 @@
 /* JavaScript */
 document.addEventListener("DOMContentLoaded", function() {
     // Chọn tất cả các phần tử cần hiển thị
-    var elementsToShow = document.querySelectorAll('.title h1, .title h2, .title h4, .invitation h3, .time_location h5, .time_location h3, .event h3, .event-time, .events-main::after, .section, .information, .content-text, .content-left,.bride-right,.bride-left,.groom-left,.groom-right,.icon,.icon-past,.past-title,.box-number,.box-left,.video-title,.video-slide video,.photo-title,.picture,.rvsp');
+    var elementsToShow = document.querySelectorAll('.title h1, .title h2, .title h4, .invitation h3, .time_location h5, .time_location h3, .event h3, .event-time, .events-main::after, .section, .information, .content-text, .content-left,.bride-right,.bride-left,.groom-left,.groom-right,.icon,.icon-past,.past-title,.box-number,.box-left,.video-title,.video-slide video,.photo-title,.picture,.rvsp, .divider');
     var line = document.querySelector('.events-main::after');
     // Lặp qua từng phần tử và thiết lập độ trễ cho mỗi phần tử
     elementsToShow.forEach(function(element, index) {
